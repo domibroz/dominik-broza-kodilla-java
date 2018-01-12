87a5cbd2d48112309af7add0796c913eeeb2f37f
Dominik Broza <domibroz@gmail.com>
2018-01-12 16:22:10 +0000

    8.4 3rd commit. Corrections.

--- kodilla-exeption/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exeption/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -4,22 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FlightSearch {
-    public static void findFlight(Flight flight)throws RouteNotFoundException{
-        Map<String , Boolean> airports = new HashMap<>();
+    public static void findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> airports = new HashMap<>();
         airports.put("GDI", true);
         airports.put("WAW", false);
-        try {
-            if(airports.get(flight.getArrivalAirport())){
-                System.out.println("You can fly from: " + flight.getDepartureAirport()+" to: " + flight.getArrivalAirport());
-            }
-            else {
+        if (airports.containsKey(flight.getArrivalAirport())) {
+            if (airports.get(flight.getArrivalAirport())) {
+                System.out.println("You can fly from: " + flight.getDepartureAirport() + " to: " + flight.getArrivalAirport());
+            } else {
                 System.out.println("You can not fly from: " + flight.getDepartureAirport() + " to: " + flight.getArrivalAirport());
-                throw new RouteNotFoundException();
+                throw new RouteNotFoundException(flight.getDepartureAirport()+" to "+ flight.getArrivalAirport());
             }
-        } catch (NullPointerException e) {
+        } else {
             System.out.println("Wrong destination!");
         }
     }
+
     public static void main(String[] args) {
         Flight flight1 = new Flight("WAW", "GDI");
         Flight flight2 = new Flight("POZ", "WAW");
@@ -27,7 +27,7 @@ public class FlightSearch {
         try {
             findFlight(fly);
         } catch (RouteNotFoundException e) {
-            System.out.println("The "+ fly.getArrivalAirport() + " airport is not available.");
+            System.out.println("The " + fly.getArrivalAirport() + " airport is not available.");
         }
 
     }

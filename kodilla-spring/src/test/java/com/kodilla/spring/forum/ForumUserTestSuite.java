0b367e7812f0edfaf409971803250043a9c5b2d4
Dominik Broza <domibroz@gmail.com>
2018-01-19 20:11:03 +0000

    9.4 2nd commit. Corrections.

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.spring.forum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class ForumUserTestSuite {
+    @Test
+    public void testGetUsername(){
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+
+        String username = forumUser.username;
+
+        Assert.assertEquals("John Smith", username);
+    }
+}

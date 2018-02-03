43c5e17d79bde08222b028880ac46be7d5284d41
Dominik Broza <domibroz@gmail.com>
2018-02-03 10:47:49 +0100

    10.1 2nd commit. Corrections.

--- kodilla-spring-training/src/test/java/com/kodilla/kodillaspringtraining/forum/ForumUserTestSuite.java
+++ kodilla-spring-training/src/test/java/com/kodilla/kodillaspringtraining/forum/ForumUserTestSuite.java
@@ -1,8 +1,7 @@
-package com.kodilla.spring.forum;
+package com.kodilla.kodillaspringtraining.forum;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,8 +9,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootTest
 public class ForumUserTestSuite {
     @Test
-    public void testGetUsername(){
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+    public void testGetUsername() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.kodillaspringtraining.forum");
         ForumUser forumUser = context.getBean(ForumUser.class);
 
         String username = forumUser.username;

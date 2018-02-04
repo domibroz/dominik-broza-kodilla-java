d316d54e4be3ccfc71402f824fa1b0af8e8f8381
Dominik Broza <domibroz@gmail.com>
2018-02-04 22:38:47 +0100

    11.1 First commit

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,15 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    @Test
+    public void testGetLastLog(){
+        Logger.getInstance().log("First log");
+
+        String actualLastLog = Logger.getInstance().getLastLog();
+
+        Assert.assertEquals("First log", actualLastLog);
+    }
+}

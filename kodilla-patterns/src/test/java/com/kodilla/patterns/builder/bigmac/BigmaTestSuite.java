bb51ae436f440ba996c0cd56a764043ca43d25e1
Dominik Broza <domibroz@gmail.com>
2018-02-07 22:14:49 +0100

    11.5 First commit

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmaTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmaTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .roll(Roll.SESAME)
+                .burgers(3)
+                .sauce(Sauce.STANDARD)
+                .ingredient(Ingredients.CUCUMBER)
+                .ingredient(Ingredients.BECON)
+                .ingredient(Ingredients.CHEESE)
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngr = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(3, howManyIngr);
+    }
+}
